e51d234d3b7ed4014b69768de8a25e8af8d045fa
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-22 11:11:51 +0500

    secondcommit

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+// const x = 12;    // practicing for javascript engine how they perform behind the scene
+
+// function newFunc(lumb) {
+//   let y = 12;
+//   if (lumb) {
+//     var nameg = "mubashir";
+//     // console.log("hi");
+//   }
+//   function anotherFunc() {
+//     const job = "teacher";
+//     console.log(nameg);
+//   }
+//   anotherFunc();
+// }
+
+// newFunc(2);
+
+// function calcAge(birthYear) {
+//   const age = 2024 - birthYear;
+//   console.log(name);
+//   return age;
+// }
+
+// const name = "jonas";
+// calcAge(1994);
+
+const name = "mubashir";
+const first = () => {
+  let a = 1;
+  const b = second(7, 9);
+  console.log(b);
+  a = a + b;
+
+  return a;
+};
+
+function second(x, y) {
+  var c = 2;
+  return c;
+}
+
+const x = first();
